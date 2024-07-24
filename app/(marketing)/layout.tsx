91786a90e7edf6b8f686db3c4edf751ce1144c2b
Swiss Tangsatjatham <tintinguy.01@gmail.com>
2024-07-24 13:28:56 +0700

    fix background display

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -9,7 +9,7 @@ const MarketingLayout = ({
     return ( 
         <div className="h-full dark:bg-[#1F1F1F]">
             <Navbar />
-            <main className="h-full pt-40 overflow-auto">
+            <main className="h-full pt-40">
                 {children}
             </main>
         </div>
